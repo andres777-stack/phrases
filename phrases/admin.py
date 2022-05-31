28f63e8c852dba12e8b56f7a4627693c19e18242
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-05-30 22:11:54 -0400

    adding PhraseAdmin

--- phrases/admin.py
+++ phrases/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Phrase
+
+@admin.register(Phrase)
+class PhraseAdmin(admin.ModelAdmin): #Para entender cómo se da esto hay que ver el __init__.py del folders admin. 
+    model = Phrase
+    list_display = ['sentence', 'author', 'created', 'updated'] #Propiedad, que tiene por defecto una tupla con __str__. 
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('created', 'updated')
+        return ()
+
 
 # Register your models here.
