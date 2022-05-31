f1bb841cbd1c58dc3cd145a96077ccb722ae99cc
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-05-31 14:58:01 -0400

    adding docs admin generator

--- wisdomPhrases/urls.py
+++ wisdomPhrases/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/docs/', include('django.contrib.admindocs.urls')), #como cualquier otra app, tiene su urls. 
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('phrases/', include('phrases.urls')),
