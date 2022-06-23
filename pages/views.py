a9f7afc1abf3684930452402b184a0802da8e1b0
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-06-22 22:48:16 -0400

    adding messages

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,18 @@
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about.html'
+
+    #def get(self, request, *args, **kwargs):
+    #    messages.debug(request, 'Debug message.')
+    #    messages.info(request, 'Info message.')
+    #    messages.success(request, 'Success message.')
+    #    messages.warning(request, 'Warning message.')
+    #    messages.error(request, 'Error message.')
+    #    return super().get(request, args, **kwargs)
 
 # Create your views here.
